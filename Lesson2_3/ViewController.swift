99c8c0e47584c92835294e688ee1aa10f970bb0d
Timur Begishev <rpsi-mono@yandex.ru>
2020-11-16 20:57:11 +0300

    ex03

---
+++ Lesson2_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2_3
+//
+//  Created by mono on 16.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
